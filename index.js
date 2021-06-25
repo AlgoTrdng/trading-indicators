ea689d56d6bbf8f91302448f83dfd9d176ea4fa4
dmc0125 <domikmic@gmail.com>
2021-06-25 13:40:26 +0200

    Fix: Exporting RSI

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const MA = require('./src/moving-average');
 const EMA = require('./src/exponential-moving-average');
 const WMA = require('./src/weighted-moving-average');
 const HMA = require('./src/hull-moving-average');
-// const rsi = require('./src/rsi');
+const RSI = require('./src/rsi');
 
 module.exports = {
   MA,
   EMA,
   WMA,
   HMA,
-  // rsi,
+  RSI,
 };
